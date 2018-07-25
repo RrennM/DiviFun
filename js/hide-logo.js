4ed783713d8257d715ab324baf026e9cdfc03f37
Tyson Hood <TysonHood@gmail.com>
2018-07-24 19:38:17 -0700

    First commit

---
+++ js/hide-logo.js
@@ -0,0 +1,7 @@
+var heroLogo = document.getElementById("hero-logo");
+var bounding = heroLogo.getBoundingClientRect();
+
+console.log(bounding);
+
+
+// bottom: 624.234375height: 400.234375left: 420right: 930.296875top: 224width: 510.296875x: 420y: 224__proto__: DOMRect
